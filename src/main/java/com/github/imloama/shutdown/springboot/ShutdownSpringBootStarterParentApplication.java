d5099fdb1b9463c5bc30feba6da21b0634828cc6
max <imloama@gmail.com>
2020-01-20 21:42:57 +0800

    init

---
+++ src/main/java/com/github/imloama/shutdown/springboot/ShutdownSpringBootStarterParentApplication.java
@@ -0,0 +1,13 @@
+package com.github.imloama.shutdown.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShutdownSpringBootStarterParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShutdownSpringBootStarterParentApplication.class, args);
+	}
+
+}
