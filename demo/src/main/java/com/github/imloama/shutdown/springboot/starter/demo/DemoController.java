afddada41307ef526e908c0cffa783030bb5acdd
maxman <imloama@gmail.com>
2020-02-02 21:38:38 +0800

    feat: demo not working

---
+++ demo/src/main/java/com/github/imloama/shutdown/springboot/starter/demo/DemoController.java
@@ -0,0 +1,19 @@
+package com.github.imloama.shutdown.springboot.starter.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() throws InterruptedException {
+        log.debug("into hello controller");
+        Thread.sleep(10000);
+        log.debug("will return hello controller");
+        return "hello,world!";
+    }
+
+}
